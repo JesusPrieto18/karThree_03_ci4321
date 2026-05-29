f96bf27fc31c7821646257d27d297b420d532422
JesusPrieto18 <jlpt.1910@gmail.com>
2026-05-29 10:11:07 -0400

    Change the repostiory name of the vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Reemplaza 'NOMBRE_DE_TU_REPOSITORIO' por el nombre exacto en GitHub
-  base: '/karThree_03_ci4321/', 
+  base: './', 
 })
